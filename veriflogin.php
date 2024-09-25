e8aab9f090942ead5933df4f575d5b6d3b0ceecb
Lucas <lucas.verbeque@gmail.com>
2024-09-25 21:47:25 +0200

    veriflogin

--- veriflogin.php
+++ veriflogin.php
@@ -4,7 +4,7 @@ session_start(); // Démarrer la session
 // Vérification si l'utilisateur est connecté
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     // Redirection vers la page de connexion si l'utilisateur n'est pas connecté
-    header('Location: login.php');
+    header('Location: /Workshop/login.php');
     exit; // Arrête l'exécution du script après la redirection
 }
 ?>
